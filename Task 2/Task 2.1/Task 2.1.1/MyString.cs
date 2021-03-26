7a3b7e0e44a1f3fae516d047f4170a8ce28909b3
PC-FILATIK-MAIN\filat <filatov23wlad@gmail.com>
2021-03-26 21:10:21 +0400

    Create Task 2.1.1

---
+++ Task 2/Task 2.1/Task 2.1.1/MyString.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class MyString
+    {
+        public char[] Symbols { get; private set; }
+        public int Length { get; private set; }
+
+        public MyString(char[] input)
+        {
+            Length = input.Length;
+            Symbols = new char[Length];
+            Symbols = input;
+        }
+
+        public bool Compare(MyString input)
+        {
+            if (Length == input.Length)
+            {
+                bool is_equally = true;
+                for (int i = 0; i < Length; i++)
+                {
+                    if (Symbols[i] != input.Symbols[i])
+                    {
+                        is_equally = false;
+                        break;
+                    }
+                }
+                if (is_equally)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Concatenation(MyString input)
+        {
+            Length += input.Length;
+            char[] str = Symbols;
+            Symbols = new char[Length];
+            int number = 0;
+            foreach (var i in str)
+            {
+                Symbols[number] = i;
+                number++;
+            }
+            foreach (var i in input.Symbols)
+            {
+                Symbols[number] = i;
+                number++;
+            }
+        }
+
+        public void Concatenation(MyString input, int index)
+        {
+            if (index < Length)
+            {
+                Length += input.Length;
+                char[] str = Symbols;
+                Symbols = new char[Length];
+                int number = 0;
+
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (index == number)
+                    {
+                        foreach (var item in input.Symbols)
+                        {
+                            Symbols[number] = item;
+                            number++;
+                        }
+                    }
+                    Symbols[number] = str[i];
+                    number++;
+                }
+            }
+            else
+            {
+                Concatenation(input);
+            }
+        }
+
+        public int NumberOfCharacter(char input)
+        {
+            int number = 0;
+            foreach (var i in Symbols)
+            {
+                if (i == input)
+                {
+                    number++;
+                }
+            }
+            return number;
+        }
+
+        public int[] IndexOfCharacter(char input)
+        {
+            int[] numbers = new int[NumberOfCharacter(input)];
+            int number = 0;
+            for (int i = 0; i < Length; i++)
+            {
+                if (Symbols[i] == input)
+                {
+                    numbers[number] = i;
+                    number++;
+                }
+            }
+            return numbers;
+        }
+
+        public char GetCharacter(int input)
+        {
+            return Symbols[input];
+        }
+
+        public void DeleteCharacter(char input)
+        {
+            Length -= NumberOfCharacter(input);
+            char[] str = Symbols;
+            Symbols = new char[Length];
+            int number = 0;
+            foreach (var i in str)
+            {
+                if (i != input)
+                {
+                    Symbols[number] = i;
+                    number++;
+                }
+            }
+        }
+
+        public void DeleteCharacter(int input)
+        {
+            if (input < Length)
+            {
+                Length -= 1;
+                char[] str = Symbols;
+                Symbols = new char[Length];
+                int number = 0;
+                for(int i = 0; i < str.Length; i++)
+                {
+                    if (i != input)
+                    {
+                        Symbols[number] = str[i];
+                        number++;
+                    }
+                }
+            }
+        }
+
+        public void ChangeCharacter(char input, int index)
+        {
+            if (index < Length)
+            {
+                Symbols[index] = input;
+            }
+        }
+
+        public override string ToString()
+        {
+            return new string(Symbols);
+        }
+    }
+}
---
+++ Task 2/Task 2.1/Task 2.1.1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyString string_1 = new MyString(new char[] { 'A', 'B', 'C' });
+            MyString string_1_1 = new MyString(new char[] { 'A', 'B', 'C' });
+            MyString string_1_2 = new MyString(new char[] { 'B', 'B', 'C' });
+
+            Console.WriteLine("Первая строка " + string_1.ToString());
+            Console.WriteLine("Вторая строка " + string_1_1.ToString());
+            Console.WriteLine("Третья строка " + string_1_2.ToString());
+
+            Console.WriteLine("Сравнение 1 и 2 " + string_1.Compare(string_1_1));
+            Console.WriteLine("Сравнение 1 и 3 " + string_1.Compare(string_1_2));
+
+            string_1.Concatenation(string_1_1);
+            Console.WriteLine("Конкатенация строк " + string_1.ToString());
+            string_1.Concatenation(string_1_2, 2);
+            Console.WriteLine("Вставка строки в определённое место " + string_1.ToString());
+
+            Console.WriteLine("Число символов D " + string_1.NumberOfCharacter('D'));
+            Console.WriteLine("Число символов B " + string_1.NumberOfCharacter('B'));
+
+            Console.Write("Индексы символа B ");
+            int[] a = string_1.IndexOfCharacter('B');
+            foreach (var i in a)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Третий символ строки " + string_1.GetCharacter(3));
+
+            string_1.DeleteCharacter(0);
+            Console.WriteLine("Удаление нулевого символа " + string_1.ToString());
+            string_1.DeleteCharacter('B');
+            Console.WriteLine("Удаление символа B " + string_1.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
