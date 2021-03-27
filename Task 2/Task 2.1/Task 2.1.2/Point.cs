7eda7ca7794d9bbec92c99c8c5e4f50feba7fe7f
PC-FILATIK-MAIN\filat <filatov23wlad@gmail.com>
2021-03-27 10:55:58 +0400

    Create Task 2.1.2

---
+++ Task 2/Task 2.1/Task 2.1.2/Point.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Point
+    {
+        private int coordinate_x;
+        private int coordinate_y;
+        public int X { get { return coordinate_x; } }
+        public int Y { get { return coordinate_y; } }
+
+        public Point(int x, int y)
+        {
+            coordinate_x = x;
+            coordinate_y = y;
+        }
+    }
+
+    public abstract class FigureWithoutArea
+    {
+        private Point point_start;
+        public Point Point_Start { get { return point_start; } }
+
+        public FigureWithoutArea(int x, int y)
+        {
+            point_start = new Point(x, y);
+        }
+
+        public virtual double Perimeter_External()
+        {
+            return 0;
+        }
+
+        public virtual void Display()
+        {
+            
+        }
+    }
+
+    public class Line : FigureWithoutArea
+    {
+        private Point point_end;
+        public Point Point_End { get { return point_end; } }
+
+        public Line(int x_s, int y_s, int x_e, int y_e) : base(x_s, y_s)
+        {
+            point_end = new Point(x_e, y_e);
+        }
+
+        public override double Perimeter_External()
+        {
+            return Math.Sqrt(Math.Pow(Point_Start.X - Point_End.X, 2) + Math.Pow(Point_Start.Y - Point_End.Y, 2));
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Линия (" + Point_Start.X + ";" + Point_Start.Y + ") (" + point_end.X + ";" + point_end.Y + ")");
+            Console.WriteLine("Длина " + Perimeter_External());
+        }
+    }
+
+    public class Circumference : FigureWithoutArea
+    {
+        private int radius;
+        public int Radius { get { return radius; } }
+
+        public Circumference(int x_s, int y_s, int r) : base(x_s, y_s)
+        {
+            radius = r;
+        }
+
+        public override double Perimeter_External()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Окружность с центром (" + Point_Start.X + ";" + Point_Start.Y + "), радиусом " + radius);
+            Console.WriteLine("Длина окружности " + Perimeter_External());
+        }
+    }
+
+    public abstract class FigureWithArea : FigureWithoutArea
+    {
+        public FigureWithArea(int x, int y) : base(x, y) 
+        { 
+        
+        }
+
+        public virtual double Area()
+        {
+            return 0;
+        }
+    }
+
+    public class Circle : FigureWithArea
+    {
+        private int radius;
+        public int Radius { get { return radius; } }
+
+        public Circle(int x_s, int y_s, int r) : base(x_s, y_s)
+        {
+            radius = r;
+        }
+
+        public override double Perimeter_External()
+        {
+            return 2* Math.PI * radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Круг с центром (" + Point_Start.X + ";" + Point_Start.Y + "), радиусом " + radius);
+            Console.WriteLine("Периметр круга " + Perimeter_External());
+            Console.WriteLine("Площадь круга " + Area());
+        }
+    }
+
+    public class Ring : Circle
+    {
+        private int radius_small;
+        public int Radius_Small { get { return radius_small; } }
+
+        public Ring(int x_s, int y_s, int r, int r_s) : base(x_s, y_s, r)
+        {
+            radius_small = r_s;
+        }
+
+        public override double Perimeter_External()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public double Perimeter_Interior()
+        {
+            return 2 * Math.PI * radius_small;
+        }
+
+        public double Perimeter_Full()
+        {
+            return Perimeter_External() + Perimeter_Full();
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radius, 2) - Math.PI * Math.Pow(radius_small, 2);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Кольцо с центром (" + Point_Start.X + ";" + Point_Start.Y + "), радиусами " + Radius + " и " + radius_small);
+            Console.WriteLine("Больший периметр " + Perimeter_External());
+            Console.WriteLine("Меньший периметр " + Perimeter_Interior());
+            Console.WriteLine("Полный периметр " + Perimeter_Full());
+            Console.WriteLine("Площадь кольца " + Area());
+        }
+    }
+
+    public class Triangle : FigureWithArea
+    {
+        private Point point_two;
+        private Point point_three;
+        public Point Point_two { get { return point_two; } }
+        public Point Point_three { get { return point_three; } }
+
+        public Triangle(int x_1, int y_1, int x_2, int y_2, int x_3, int y_3) : base(x_1, y_1)
+        {
+            point_two = new Point(x_2, y_2);
+            point_three = new Point(x_3, y_3);
+        }
+
+        public override double Perimeter_External()
+        {
+            return Math.Sqrt(Math.Pow(Point_Start.X - point_two.X, 2) + Math.Pow(Point_Start.Y - point_two.Y, 2)) +
+                Math.Sqrt(Math.Pow(point_two.X - point_three.X, 2) + Math.Pow(point_two.Y - point_three.Y, 2)) +
+                Math.Sqrt(Math.Pow(point_three.X - Point_Start.X, 2) + Math.Pow(point_three.Y - Point_Start.Y, 2));
+        }
+
+        public override double Area()
+        {
+            double semi_periemeter = Perimeter_External() / 2;
+            return Math.Sqrt(semi_periemeter * 
+                (semi_periemeter - Math.Sqrt(Math.Pow(Point_Start.X - point_two.X, 2) + Math.Pow(Point_Start.Y - point_two.Y, 2))) * 
+                (semi_periemeter - Math.Sqrt(Math.Pow(point_two.X - point_three.X, 2) + Math.Pow(point_two.Y - point_three.Y, 2))) * 
+                (semi_periemeter - Math.Sqrt(Math.Pow(point_three.X - Point_Start.X, 2) + Math.Pow(point_three.Y - Point_Start.Y, 2))));
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Треугольник (" + Point_Start.X + ";" + Point_Start.Y + ") (" + point_two.X + ";" + point_two.Y + ") (" + point_three.X + ";" + point_three.Y + ")");
+            Console.WriteLine("Периметр " + Perimeter_External());
+            Console.WriteLine("Площадь " + Area());
+        }
+    }
+
+    public class Square : FigureWithArea
+    {
+        private int side_height;
+        public int Side_Height { get { return side_height; } }
+
+        public Square(int x_s, int y_s, int s_h) : base(x_s, y_s)
+        {
+            side_height = s_h;
+        }
+
+        public override double Perimeter_External()
+        {
+            return 4 * side_height;
+        }
+
+        public override double Area()
+        {
+            return Math.Pow(side_height, 2);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Квадрат (нижняя левая точка (" + Point_Start.X + ";" + Point_Start.Y + ")), сторона квадрат" + side_height);
+            Console.WriteLine("Периметр " + Perimeter_External());
+            Console.WriteLine("Площадь " + Area());
+        }
+    }
+
+    public class Rectangle : Square
+    {
+        private int side_width;
+        public int Side_Width { get { return side_width; } }
+
+        public Rectangle(int x_s, int y_s, int s_h, int s_w) : base(x_s, y_s, s_h)
+        {
+            side_width = s_w;
+        }
+
+        public override double Perimeter_External()
+        {
+            return 4 * (Side_Height + side_width);
+        }
+
+        public override double Area()
+        {
+            return side_width * Side_Height;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Прямоугольник (нижняя левая точка (" + Point_Start.X + ";" + Point_Start.Y + ")), высота " + Side_Height + ", ширина " + side_width);
+            Console.WriteLine("Периметр " + Perimeter_External());
+            Console.WriteLine("Площадь " + Area());
+        }
+    }
+}
---
+++ Task 2/Task 2.1/Task 2.1.2/Program.cs
@@ -0,0 +1,594 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Programa p = new Programa();
+            p.Main_Menu();
+        }
+    }
+
+    public class Programa
+    {
+        Canvas canvas = new Canvas();
+
+        public void Main_Menu()
+        {
+            int input = 0;
+            do
+            {
+                Console.WriteLine("Выберите действие");
+                Console.WriteLine("1. Добавить фигуру");
+                Console.WriteLine("2. Вывести фигуры");
+                Console.WriteLine("3. Очистить холст");
+                Console.WriteLine("4. Выход");
+                try
+                {
+                    input = Int32.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    input = 0;
+                }
+            } while (input < 1 || input > 4);
+            switch (input)
+            {
+                case 1:
+                    Select_Figure();
+                    break;
+                case 2:
+                    canvas.Show_Figures();
+                    Main_Menu();
+                    break;
+                case 3:
+                    canvas.Clear_Canvas();
+                    Main_Menu();
+                    break;
+                case 4: break;
+                default: break;
+            }
+        }
+
+        public void Select_Figure()
+        {
+            int input = 0;
+            do
+            {
+                Console.WriteLine("Выберите фигуру");
+                Console.WriteLine("1. Добавить линию");
+                Console.WriteLine("2. Добавить окружность");
+                Console.WriteLine("3. Добавить круг");
+                Console.WriteLine("4. Добавить кольцо");
+                Console.WriteLine("5. Добавить треугольник");
+                Console.WriteLine("6. Добавить квадрат");
+                Console.WriteLine("7. Добавить прямоугольник");
+                Console.WriteLine("8. Выход");
+                try
+                {
+                    input = Int32.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    input = 0;
+                }
+            } while (input < 1 || input > 8);
+            switch (input)
+            {
+                case 1:
+                    canvas.Add_Line();
+                    Main_Menu();
+                    break;
+                case 2:
+                    canvas.Add_Circumference();
+                    Main_Menu();
+                    break;
+                case 3:
+                    canvas.Add_Circle();
+                    Main_Menu();
+                    break;
+                case 4:
+                    canvas.Add_Ring();
+                    Main_Menu();
+                    break;
+                case 5:
+                    canvas.Add_Triangle();
+                    Main_Menu();
+                    break;
+                case 6:
+                    canvas.Add_Square();
+                    Main_Menu();
+                    break;
+                case 7:
+                    canvas.Add_Rectangle();
+                    Main_Menu();
+                    break;
+                case 8:
+                    Main_Menu();
+                    break;
+                default: break;
+            }
+        }
+    }
+
+    public class Canvas
+    {
+        private List<FigureWithoutArea> figures = new List<FigureWithoutArea>();
+
+        public void Add_Line()
+        {
+            bool next = false;
+            int input_x_1 = 0;
+            int input_y_1 = 0;
+            int input_x_2 = 0;
+            int input_y_2 = 0;
+            do
+            {
+                Console.WriteLine("Ввведите координату X начала прямой");
+                try
+                {
+                    input_x_1 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите координату Y начала прямой");
+                try
+                {
+                    input_y_1 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите координату X конца прямой");
+                try
+                {
+                    input_x_2 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите координату Y конца прямой");
+                try
+                {
+                    input_y_2 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            figures.Add(new Line(input_x_1, input_y_1, input_x_2, input_y_2));
+            Console.WriteLine("Линия создана");
+        }
+
+        public void Add_Circumference()
+        {
+            bool next = false;
+            int input_x_1 = 0;
+            int input_y_1 = 0;
+            int input_r = 0;
+            do
+            {
+                Console.WriteLine("Ввведите координату X центра окружности");
+                try
+                {
+                    input_x_1 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите координату X центра окружности");
+                try
+                {
+                    input_y_1 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите радиус окружности");
+                try
+                {
+                    input_r = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            figures.Add(new Circumference(input_x_1, input_y_1, input_r));
+            Console.WriteLine("Окружность создана");
+        }
+
+        public void Add_Circle()
+        {
+            bool next = false;
+            int input_x_1 = 0;
+            int input_y_1 = 0;
+            int input_r = 0;
+            do
+            {
+                Console.WriteLine("Ввведите координату X центра круга");
+                try
+                {
+                    input_x_1 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите координату X центра круга");
+                try
+                {
+                    input_y_1 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите радиус круга");
+                try
+                {
+                    input_r = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            figures.Add(new Circle(input_x_1, input_y_1, input_r));
+            Console.WriteLine("Круг создан");
+        }
+
+        public void Add_Ring()
+        {
+            bool next = false;
+            int input_x_1 = 0;
+            int input_y_1 = 0;
+            int input_r = 0;
+            int input_r_s = 0;
+            do
+            {
+                Console.WriteLine("Ввведите координату X центра кольца");
+                try
+                {
+                    input_x_1 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите координату Y центра кольца");
+                try
+                {
+                    input_y_1 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите больший радиус кольца");
+                try
+                {
+                    input_r = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите меньший радиус кольца");
+                try
+                {
+                    input_r_s = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            figures.Add(new Ring(input_x_1, input_y_1, input_r, input_r_s));
+            Console.WriteLine("Кольцо создано");
+        }
+
+        public void Add_Triangle()
+        {
+            bool next = false;
+            int input_x_1 = 0;
+            int input_y_1 = 0;
+            int input_x_2 = 0;
+            int input_y_2 = 0;
+            int input_x_3 = 0;
+            int input_y_3 = 0;
+            do
+            {
+                Console.WriteLine("Ввведите координату X первой точки");
+                try
+                {
+                    input_x_1 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите координату Y первой точки");
+                try
+                {
+                    input_y_1 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите координату X второй точки");
+                try
+                {
+                    input_x_2 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите координату Y второй точки");
+                try
+                {
+                    input_y_2 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите координату X третьей точки");
+                try
+                {
+                    input_x_3 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите координату Y третьей точки");
+                try
+                {
+                    input_y_3 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            figures.Add(new Triangle(input_x_1, input_y_1, input_x_2, input_y_2, input_x_3, input_y_3));
+            Console.WriteLine("Треугольник создан");
+        }
+
+        public void Add_Square()
+        {
+            bool next = false;
+            int input_x_1 = 0;
+            int input_y_1 = 0;
+            int input_a = 0;
+            do
+            {
+                Console.WriteLine("Ввведите координату X левой нижней точки квадрата");
+                try
+                {
+                    input_x_1 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите координату X левой нижней точки квадрата");
+                try
+                {
+                    input_y_1 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите сторону квадрата");
+                try
+                {
+                    input_a = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            figures.Add(new Square(input_x_1, input_y_1, input_a));
+            Console.WriteLine("Квадрат создан");
+        }
+
+        public void Add_Rectangle()
+        {
+            bool next = false;
+            int input_x_1 = 0;
+            int input_y_1 = 0;
+            int input_h = 0;
+            int input_w = 0;
+            do
+            {
+                Console.WriteLine("Ввведите координату X левой нижней точки прямоугольника");
+                try
+                {
+                    input_x_1 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите координату Y левой нижней точки прямоугольника");
+                try
+                {
+                    input_y_1 = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Ввведите высотц прямоугольника");
+                try
+                {
+                    input_h = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            next = false;
+            do
+            {
+                Console.WriteLine("Введите ширину прямоугольника");
+                try
+                {
+                    input_w = Int32.Parse(Console.ReadLine());
+                    next = true;
+                }
+                catch
+                {
+                    next = false;
+                }
+            } while (!next);
+            figures.Add(new Rectangle(input_x_1, input_y_1, input_h, input_w));
+            Console.WriteLine("Прямоугольник создан");
+        }
+
+        public void Show_Figures()
+        {
+            if (figures == null || figures.Count == 0)
+            {
+                Console.WriteLine("Холст пустой");
+            }
+            else
+            {
+                Console.WriteLine("Фигуры на холсте");
+                for (int i = 0; i < figures.Count; i++)
+                {
+                    Console.WriteLine(i + 1 + " -----");
+                    figures[i].Display();
+                }
+            }
+        }
+
+        public void Clear_Canvas()
+        {
+            Console.WriteLine("Холст очищен");
+            figures = null;
+        }
+    }
+}
