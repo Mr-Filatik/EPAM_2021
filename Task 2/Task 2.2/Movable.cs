399deff6f6df02c1e6c0a853601d4715d465def6
PC-FILATIK-MAIN\filat <filatov23wlad@gmail.com>
2021-03-28 12:08:57 +0400

    Create Task 2.2

---
+++ Task 2/Task 2.2/Movable.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Bonus
+    {
+        private int coordinate_x;
+        private int coordinate_y;
+        //private int value;
+        private char icon;
+        public int X { get { return coordinate_x; } }
+        public int Y { get { return coordinate_y; } }
+        //public int Value { get { return value; } }
+        public char Icon { get { return icon; } }
+
+        public Bonus(int v_x, int v_y /*,int v*/)
+        {
+            coordinate_x = v_x;
+            coordinate_y = v_y;
+            //value = v;
+            icon = 'B';
+        }
+    }
+
+    public class Being
+    {
+        private int coordinate_x;
+        private int coordinate_y;
+        private int hp;
+        private char icon;
+        private bool[] free;
+        public int X { get { return coordinate_x; } }
+        public int Y { get { return coordinate_y; } }
+        public int HP { get { return hp; } }
+        public char Icon { get { return icon; } }
+
+        public Being(int v_x, int v_y, int v_hp, char v_i)
+        {
+            coordinate_x = v_x;
+            coordinate_y = v_y;
+            hp = v_hp;
+            icon = v_i;
+        }
+
+        public void Move(int vertical, int horizontal)
+        {
+            if ((free[0] == true && vertical < 0)||(free[2] == true && vertical > 0))
+            {
+                coordinate_y += vertical;
+            }
+            if ((free[1] == true && horizontal > 0) || (free[3] == true && horizontal < 0))
+            {
+                coordinate_x += horizontal;
+            }
+        }
+
+        public void Set_Free(bool[] f)
+        {
+            free = f;
+        }
+
+        public void HPController(int d)
+        {
+            hp -= d;
+            if(hp < 0)
+            {
+                hp = 0;
+            }
+        }
+    }
+
+    public class Enemy : Being
+    {
+        private int damage;
+        public int Damage { get { return damage; } }
+
+        public Enemy(int x, int y, int hp, int d) : base(x, y, hp, 'E')
+        {
+            damage = d;
+        }
+
+        public void Random_Direction(int a, int b)
+        {
+            if (a == 0)
+            {
+                Move(b, 0);
+            }
+            else
+            {
+                Move(0, b);
+            }
+        }
+    }
+
+    public class Player : Being
+    {
+        public Player(int x, int y, int hp) : base(x, y, hp, 'P')
+        {
+            
+        }
+
+        public void Input()
+        {
+            ConsoleKey key = Console.ReadKey().Key;
+            if (key == ConsoleKey.W)
+            {
+                Move(-1, 0);
+            }
+            if (key == ConsoleKey.S)
+            {
+                Move(1, 0);
+            }
+            if (key == ConsoleKey.D)
+            {
+                Move(0, 1);
+            }
+            if (key == ConsoleKey.A)
+            {
+                Move(0, -1);
+            }
+        }
+    }
+}
---
+++ Task 2/Task 2.2/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            World world = new World();
+            Console.WriteLine("Enter E for start game (control - wasd)");
+            Console.WriteLine("Another button exit");
+            ConsoleKey key = Console.ReadKey().Key;
+            do
+            {
+                world.Create_World();
+                if (world.Play())
+                {
+                    Console.Clear();
+                    Console.WriteLine("You Win!!!");
+                    Console.WriteLine("Enter E for restart game (control - wasd)");
+                    Console.WriteLine("Another button exit");
+                    key = Console.ReadKey().Key;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("You dead!!!");
+                    Console.WriteLine("Enter E for restart game (control - wasd)");
+                    Console.WriteLine("Another button exit");
+                    key = Console.ReadKey().Key;
+                }
+            } while (key == ConsoleKey.E);
+            Console.ReadKey();
+        }
+    }
+
+    public class World
+    {
+        private Map map;
+        private Player player;
+        private List<Enemy> enemies;
+        private List<Bonus> bonuses;
+
+        public void Create_World()
+        {
+            map = new Map(10, 10);
+            player = new Player(2, 2, 3);
+            enemies = new List<Enemy>();
+            enemies.Add(new Enemy(8, 8, 10, 1));
+            enemies.Add(new Enemy(2, 8, 10, 1));
+            enemies.Add(new Enemy(8, 2, 10, 1));
+            enemies.Add(new Enemy(6, 6, 10, 1));
+            bonuses = new List<Bonus>();
+            bonuses.Add(new Bonus(1, 9));
+            bonuses.Add(new Bonus(1, 1));
+            bonuses.Add(new Bonus(8, 5));
+            bonuses.Add(new Bonus(9, 9));
+        }
+
+        public bool Play()
+        {
+            Random random = new Random();
+            map.Draw_Map(player, enemies, bonuses);
+            do
+            {
+                player.Set_Free(map.Enviroment(player.X, player.Y));
+
+                player.Input();
+
+                for (int i = 0; i < enemies.Count; i++)
+                {
+                    if (player.X == enemies[i].X && player.Y == enemies[i].Y)
+                    {
+                        player.HPController(enemies[i].Damage);
+                        enemies.RemoveAt(i);
+                    }
+                }
+
+                for (int i = 0; i < bonuses.Count; i++)
+                {
+                    if (player.X == bonuses[i].X && player.Y == bonuses[i].Y)
+                    {
+                        bonuses.RemoveAt(i);
+                    }
+                }
+
+                foreach (var enemy in enemies)
+                {
+                    enemy.Set_Free(map.Enviroment(enemy.X, enemy.Y));
+                    enemy.Random_Direction(random.Next(0, 2), random.Next(-1, 2));
+                }
+
+                map.Draw_Map(player, enemies, bonuses);
+
+                if (player.HP == 0)
+                {
+                    player = null;
+                }
+            } while (player != null && bonuses.Count > 0);
+            if (player == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+
+    public class Map
+    {
+        private int height;
+        private int width;
+        private char[,] map;
+        public Map(int h, int w)
+        {
+            height = h;
+            width = w;
+            map = new char[10, 10]
+            {
+                {' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ' },
+                {' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ' },
+                {' ', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ' },
+                {'#', '#', '#', '#', ' ', ' ', '#', ' ', ' ', ' ' },
+                {' ', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ' },
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+            };
+        }
+
+        public void Draw_Map(Player player, List<Enemy> enemies, List<Bonus> bonuses)
+        {
+            Console.Clear();
+            for (int h = -1; h <= height; h++)
+            {
+                for (int w = -1; w <= width; w++)
+                {
+                    if (h == -1 || w == -1 || h == height || w == width)
+                    {
+                        Console.Write('#');
+                    }
+                    else
+                    {
+                        char icon = ' ';
+                        bool draw = true;
+                        if (h == player.Y && w == player.X)
+                        {
+                            icon = player.Icon;
+                            draw = false;
+                        }
+                        foreach (var enemy in enemies)
+                        {
+                            if (h == enemy.Y && w == enemy.X)
+                            {
+                                icon = enemy.Icon;
+                                draw = false;
+                                break;
+                            }
+                        }
+                        foreach (var bonus in bonuses)
+                        {
+                            if (h == bonus.Y && w == bonus.X)
+                            {
+                                icon = bonus.Icon;
+                                draw = false;
+                                break;
+                            }
+                        }
+                        if (draw)
+                        {
+                            Console.Write(map[h, w]);
+                        }
+                        else
+                        {
+                            Console.Write(icon);
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Health player: " + player.HP);
+            Console.WriteLine("Bonuses to win: " + bonuses.Count);
+        }
+        
+        public bool[] Enviroment(int x, int y)
+        {
+            bool[] res = new bool[4] { false, false, false, false};
+            if (y > 0 && map[y - 1, x] == ' ')
+            {
+                res[0] = true;
+            }
+            if (y < height - 1 && map[y + 1, x] == ' ')
+            {
+                res[2] = true;
+            }
+            if (x < width - 1 && map[y, x + 1] == ' ')
+            {
+                res[1] = true;
+            }
+            if (x > 0 && map[y, x - 1] == ' ')
+            {
+                res[3] = true;
+            }
+            return res;
+        }
+    }
+
+
+}
