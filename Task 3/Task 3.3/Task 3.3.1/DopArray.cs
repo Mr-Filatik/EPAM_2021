d3956db7fe2108b6fcf29921e5f7040fde767ac7
PC-FILATIK-MAIN\filat <filatov23wlad@gmail.com>
2021-04-17 11:50:28 +0400

    Create Task 3.3.1

---
+++ Task 3/Task 3.3/Task 3.3.1/DopArray.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public static class DopArray
+    {
+        public static void ElementSquare(this int[] input, int number)
+        {
+            if (number >= 0 && number < input.Length)
+            {
+                input[number] *= input[number];
+            }
+            else
+            {
+                Console.WriteLine("EXSEPTION");
+            }
+        }
+
+        public static void ElementPlusPlus(this int[] input, int number)
+        {
+            if (number >= 0 && number < input.Length)
+            {
+                input[number]++;
+            }
+            else
+            {
+                Console.WriteLine("EXSEPTION");
+            }
+        }
+
+        public static int SumArray(this int[] input)
+        {
+            int sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                sum += input[i];
+            }
+            return sum;
+        }
+
+        public static float AverArray(this int[] input)
+        {
+            float aver = 0;
+            aver = input.SumArray() / input.Length;
+            return aver;
+        }
+
+        public static int MaxVisArray(this int[] input)
+        {
+            int number = 0;
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int c = 0;
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (input[j] == input[i]) { 
+                        c++; 
+                    }
+                }
+                if (count < c)
+                {
+                    count = c;
+                    number = i;
+                }
+            }
+            return input[number];
+        }
+    }
+}
---
+++ Task 3/Task 3.3/Task 3.3.1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 2 };
+
+            Console.WriteLine("Sum " + arr.SumArray());
+            Console.WriteLine("Aver " + arr.AverArray());
+            Console.WriteLine("MaxVis " + arr.MaxVisArray());
+
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            arr.ElementPlusPlus(2);
+
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            arr.ElementSquare(1);
+
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+        //public delegate void Act(this int[] input, int number);
+        //public delegate void Act(int number);
+    }
+}
